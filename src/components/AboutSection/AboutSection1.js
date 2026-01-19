dbc53b91b15c6351bf4638c9eb70efd37dbe0bf6
JatinGautam13 <166696883+JatinGautam13@users.noreply.github.com>
2026-01-19 18:04:27 +0000

    sh

--- src/components/AboutSection/AboutSection1.js
+++ src/components/AboutSection/AboutSection1.js
@@ -25,6 +25,7 @@ export default function AboutSection1() {
           {/* <hello></hello> */}
           {/* <hello></hello> */}
           {/* <hello></hello> */}
+          {/* <hello></hello> */}
 
         </div>
         <div className={styles.middle}>
